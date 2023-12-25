6704c24fa8eb65b8ace7f36bd137eda00d842ae1
Sonu12321 <82197012+Sonu12321@users.noreply.github.com>
2023-12-25 17:44:29 +0000

    operation

---
+++ basics/operation.js
@@ -0,0 +1,8 @@
+console.log(2**5);
+let str = 3
+let str2 = 2
+let str4 = str + str2
+console.log(str4);
+console.log(3 + 2 + "3");
+console.log("3" + 2 + 3);//it rule to line
+console.log(+true);//not to do like this
