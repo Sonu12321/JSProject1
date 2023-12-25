78254b9c35deba68d3d0dedb76387e3596da4f1b
Sonu12321 <82197012+Sonu12321@users.noreply.github.com>
2023-12-25 17:47:18 +0000

    comparing

---
+++ basics/compare.js
@@ -0,0 +1,4 @@
+console.log("2"===1);
+console.log(null==0);
+console.log(null>0);//in js comparrison have diff rules as "<=" it conversts null to 0 and else its always null
+console.log(null>=0);//avoid this type of comparrison
