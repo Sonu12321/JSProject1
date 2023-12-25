ba2fb1bb06199deb775fbe8b69c3c35801676709
Sonu12321 <82197012+Sonu12321@users.noreply.github.com>
2023-12-25 17:18:00 +0000

    conversion of data

---
+++ basics/test.js
@@ -0,0 +1,10 @@
+let value = "54"
+let convert = Number(value)
+let bollean ;
+let co = Boolean(bollean)
+console.log(co)
+let a = "#@$ @$"
+let b = String(a);
+console.log(b);
+console.log(typeof(b));
+//we can convert using Number for numbers,String for 
