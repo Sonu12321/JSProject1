c4facbe34e4619da286e9ec5bc6aa2f5f6ce845a
Sonu12321 <82197012+Sonu12321@users.noreply.github.com>
2023-12-25 18:07:37 +0000

    datatypes

---
+++ basics/datatpes.js
@@ -0,0 +1,16 @@
+//primitive
+//string,number,bool,null(empty),undefined,symbolBigint(scientific values)
+//reference type(non primitive)=>arrays,Objects,function
+//is js dynamic or static typed =>dynaml
+const isLoggedIn =false
+const n = ["a","b","v"];
+console.log(typeof(n));
+let myname = 
+{
+    age =22
+}
+console.log(typeof(myname));
+console.log(isLoggedIn);
+let Id = Symbol("1213");
+let aId = Symbol("1213");
+console.log(aId===Id);
