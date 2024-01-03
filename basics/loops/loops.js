ad9e4c277ccdd835cb24f9697a2dba219e80dc4c
Sonu12321 <82197012+Sonu12321@users.noreply.github.com>
2024-01-03 15:45:42 +0000

    maps and loops

---
+++ basics/loops/loops.js
@@ -0,0 +1,15 @@
+let isarray = ['hello','he','she']
+let arr = 0
+while(arr<isarray.length){
+
+ //   console.log(`my array is ${isarray[arr]}`);
+    arr=arr+1
+}
+//forof loop
+const s = [1,2,3,35]
+for (const num of s ) {
+    let num = 0
+    if (num<arr.length){
+        console.log(num);
+}
+}
---
+++ basics/loops/map.js
@@ -0,0 +1,8 @@
+const map = new Map()
+map.set('IN',"india")
+map.set('Ia',"indiasdasda")
+map.set('Is',"bafa")
+for (const [key,value] of map) {
+  console.log(key,':-',value);  
+}
+//console.log(map);
